92a3408518d6c06f69a623172a0e171621d8f9ab
PanHongMing <40025146@gm.nfu.edu.tw>
2018-06-14 13:58:55 +0800

    修正註解

--- Client Event/Relationship Event/filterSearchDialog.js
+++ Client Event/Relationship Event/filterSearchDialog.js
@@ -5,5 +5,6 @@ var criteria="";
 for(var i=0;i<members.length;i++){
     criteria=criteria+members[i].selectSingleNode("related_id/Item[@type='Identity']").getElementsByTagName("name")[0].text+"|";
 }
+//retObj[指定欄位名稱]={filterValue=此欄位的值}
 retObj["name"] = { filterValue:criteria, isFilterFixed:true };
 return retObj;
